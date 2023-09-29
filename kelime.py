b97711e27b17225daeb062c4cc00b6b2fb1ff88d
susukin0 <70662829+susukin0@users.noreply.github.com>
2023-09-29 22:39:29 +0300

    Create kelime.py

---
+++ kelime.py
@@ -0,0 +1,10 @@
+
+ekleme = input("liste giriniz: ")
+
+kelimeler = ekleme.split()
+
+                                                        list = []
+for kelime in kelimeler:
+    list.append(kelime)
+
+print(list)
